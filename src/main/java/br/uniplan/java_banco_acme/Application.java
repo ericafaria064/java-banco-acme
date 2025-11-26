fdb1b77354b5828d8f3254f93403d394471cec3b
Erica Faria <fariaerica064@gmail.com>
2025-11-25 22:09:27 -0300

    Set up initial Gradle project structure with Java support, including application and test classes, configuration files, and wrapper scripts.

---
+++ src/main/java/br/uniplan/java_banco_acme/Application.java
@@ -0,0 +1,13 @@
+package br.uniplan.java.banco.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBancoAcmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBancoAcmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uniplan/java_banco_acme/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uniplan.java.banco.acme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBancoAcmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
